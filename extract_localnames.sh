1870c09d76fd99089c01943c4d5955c113fa2535
Jozef Mlich <jmlich83@gmail.com>
2022-04-18 10:42:56 +0200

    Use the same QObject in main.cpp

--- extract_localnames.sh
+++ extract_localnames.sh
@@ -2,18 +2,18 @@
 
 declare -A list=(
     [glacier-alarmclock]='string(//context[name="QObject"]/message[source="Clock"]/translation)'
-    [glacier-browser]='string(//context[name="MainPage"]/message[source="Browser"]/translation)'
-    [glacier-calc]='string(//context[name="glacier-calc"]/message[source="Calculator"]/translation)'
-    [glacier-camera]='string(//context[name="CameraPage"]/message[source="Camera"]/translation)'
+    [glacier-browser]='string(//context[name="QObject"]/message[source="Browser"]/translation)'
+    [glacier-calc]='string(//context[name="QObject"]/message[source="Calculator"]/translation)'
+    [glacier-camera]='string(//context[name="QObject"]/message[source="Camera"]/translation)'
     [glacier-contacts]='string(//context[name="QObject"]/message[source="Contacts"]/translation)'
-    [glacier-dialer]='string(//context[name="FirstPage"]/message[source="Dialer"]/translation)'
+    [glacier-dialer]='string(//context[name="QObject"]/message[source="Dialer"]/translation)'
     [glacier-filemuncher]='string(//context[name="QObject"]/message[source="Files browser"]/translation)'
     [glacier-gallery]='string(//context[name="QObject"]/message[source="Gallery"]/translation)'
-    [glacier-music]='string(//context[name="PlayerPage"]/message[source="Music"]/translation)'
+    [glacier-music]='string(//context[name="QObject"]/message[source="Music"]/translation)'
     [glacier-packagemanager]='string(//context[name="QObject"]/message[source="Packages"]/translation)'
     [glacier-settings]='string(//context[name="QObject"]/message[source="Settings"]/translation)'
     [glacier-testtool]='string(//context[name="QObject"]/message[source="Hardware test"]/translation)'
-    [glacier-weather]='string(//context[name="MainPage"]/message[source="Weather"]/translation)'
+    [glacier-weather]='string(//context[name="QObject"]/message[source="Weather"]/translation)'
     [glacier-messages]='string(//context[name="QObject"]/message[source="Messages"]/translation)'
     [glacier-calendar]='string(//context[name="QObject"]/message[source="Calendar"]/translation)'
 )
@@ -23,6 +23,7 @@ cd ..
 for repo in ${!list[@]}; do
     query="${list[$repo]}"
 
+if /bin/false; then
     cd ./$repo
         git fetch --all --tags
         git merge upstream/master
@@ -30,6 +31,7 @@ for repo in ${!list[@]}; do
         ts=$(find . -name "$repo.ts")
         lupdate . -recursive -ts "$ts"
     cd ..
+fi
 
     desktop_file=$(find $repo -name "$repo"'.desktop')
     count=0
@@ -41,7 +43,7 @@ for repo in ${!list[@]}; do
         fi
 
         local_name=$(xmllint --xpath "$query" "$fn")
-#        echo "$repo Name[$lang]=$local_name"
+        echo "$repo Name[$lang]=$local_name"
 
         if [ -z "$local_name" ]; then
             continue
